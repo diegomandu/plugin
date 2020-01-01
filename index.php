a1d8d8fff8dbf296207344c20adc16b35fa9b946
Diego Mandu <dyeguitto@hotmail.com>
2020-01-01 19:49:40 -0300

    Creating the header meta for the plugin

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+*	Plugin Name: Recipe
+*	Description: A simple WordPress plugin that allows user to create recipes and rate those   recipes
+*	Version: 1.0
+*	Author: Diego Mandu
+*	Author URI: http://mandudev.com/
+* 	text domain: recipe
+*/
