9629afac6c5ab4a0815026e7542cd3b341beb2f2
Diego Mandu <dyeguitto@hotmail.com>
2020-01-02 20:56:31 -0300

    including str_replace to make the placeholder dynamically

--- process/filter-content.php
+++ process/filter-content.php
@@ -5,7 +5,11 @@ function r_filter_recipe_content( $content ){
 		return $content;
 	}
 
+	global $post;
+	$recipe_data			=	get_post_meta( $post->ID, 'recipe-data', true );
 	$recipe_html			=	file_get_contents( 'recipe-template.php', true );
+	$recipe_html			=	str_replace( 'RATE_I18N', __("Rating", "recipe"), $recipe_html );
+	
 
 	return $recipe_html . $content;
 	
