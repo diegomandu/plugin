07d465b02d82a6123a15f3a9df3622f54c8a6d1f
Diego Mandu <dyeguitto@hotmail.com>
2020-01-04 12:41:01 -0300

    Register and enqueue rateit.css/js and main.js

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -1,5 +1,20 @@
 <?php
 
 function r_enqueue_scripts(){
-	
+	wp_register_style( 'r_rateit', plugins_url( '/assets/rateit/rateit.css', RECIPE_PLUGIN_URL ) );
+	wp_enqueue_style( 'r_rateit' );
+
+	wp_register_script(
+		'r_rateit',
+		plugins_url( '/assets/rateit/jquery.rateit.min.js', RECIPE_PLUGIN_URL ),
+		['jquery'],
+		'1.0.0',
+		true
+	);
+	wp_register_script(
+		'r_main', plugin_url( '/assets/js/main.js', RECIPE_PLUGIN_URL ), ['jquery'], '1.0.0', true
+	);
+
+	wp_enqueue_script( 'r_rateit' );
+	wp_enqueue_script( 'r_main' );
 }
