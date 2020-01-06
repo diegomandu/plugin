2f9c99d6a532e7c1856b0fcf691b643fd723bb4b
Diego Mandu <dyeguitto@hotmail.com>
2020-01-06 11:48:24 -0300

    Add function r_rate_recipe deal with ajax response of rating

--- process/rate-recipe.php
+++ process/rate-recipe.php
@@ -1,7 +1,14 @@
 <?php 
 
 function r_rate_recipe(){
-	print_r($_POST);
+	global $wpdb;
+
+	$output			=	[ 'status' => 1 ];
+	$post_ID		=	absint( $_POST['rid'] );
+	$rating			=	round( $_POST['rating'], 1 );
+	$user_IP		=	$_SERVER['REMOTE_ADDR'];
+
+
 }
 
 ?>
