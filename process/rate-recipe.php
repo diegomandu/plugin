22bc313b2a5298ed24010f6edd80136b16e617a7
Diego Mandu <dyeguitto@hotmail.com>
2020-01-06 13:55:46 -0300

    inserted the rating in the database and sent response

--- process/rate-recipe.php
+++ process/rate-recipe.php
@@ -8,7 +8,18 @@ function r_rate_recipe(){
 	$rating			=	round( $_POST['rating'], 1 );
 	$user_IP		=	$_SERVER['REMOTE_ADDR'];
 
+	$wpdb->insert(
+		$wpdb->prefix . 'recipe_ratings',
+		[
+			'recipe_id' =>	$post_ID,
+			'rating'	=>	$rating,
+			'user_ip'	=>	$user_IP
+		],
+		['%d', '%f', '%s']
+	);
 
+	$output['status']	=	2;
+	wp_send_json( $output );
 }
 
 ?>
