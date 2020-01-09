4d35a40a72507bb131072e91bed78e01a5e2f6e9
Diego Mandu <dyeguitto@hotmail.com>
2020-01-09 13:30:54 -0300

    add action manage_recipe_posts_custom_column

--- includes/admin/init.php
+++ includes/admin/init.php
@@ -4,4 +4,5 @@ function recipe_admin_init(){
 	include( 'column.php' );
 
 	add_filter( 'manage_recipe_posts_columns', 'r_add_new_recipe_columns' );
+	add_action( 'manage_recipe_posts_custom_column', 'r_manage_recipe_columns', 10, 2 );
 }
