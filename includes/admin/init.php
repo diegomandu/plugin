68d93d1f02612c233345d9774854bd4bfaeef4b1
Diego Mandu <dyeguitto@hotmail.com>
2020-01-09 13:27:36 -0300

    Fixing typo inside manage_recipe_posts_columns filter

--- includes/admin/init.php
+++ includes/admin/init.php
@@ -3,5 +3,5 @@
 function recipe_admin_init(){
 	include( 'column.php' );
 
-	add_filter( 'manage_recipe_posts_column', 'r_add_new_recipe_columns' );
+	add_filter( 'manage_recipe_posts_columns', 'r_add_new_recipe_columns' );
 }
