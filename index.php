6088c2efef7c4afb66bf2cce5abc2973ff64343c
Diego Mandu <dyeguitto@hotmail.com>
2020-01-10 18:55:33 -0300

    Include includes/cron.php

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ include( 'process/rate-recipe.php' );
 include( 'includes/admin/init.php' );
 include( dirname(RECIPE_PLUGIN_URL) . '/includes/widgets.php' );
 include( 'includes/widgets/daily-recipe.php' );
+include( 'includes/cron.php' );
 
 // Hooks
 register_activation_hook( __FILE__, 'r_activate_plugin' );
