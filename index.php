49ef6909d3c4497dd9d216fa2570516708eb2869
Diego Mandu <dyeguitto@hotmail.com>
2020-01-10 19:07:32 -0300

    Include deactivate.php

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ include( 'includes/admin/init.php' );
 include( dirname(RECIPE_PLUGIN_URL) . '/includes/widgets.php' );
 include( 'includes/widgets/daily-recipe.php' );
 include( 'includes/cron.php' );
+include( 'includes/deactivate.php' );
 
 // Hooks
 register_activation_hook( __FILE__, 'r_activate_plugin' );
