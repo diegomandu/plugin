0646d722c8f22b75d2dde062a5a5f6e184a4ff60
Diego Mandu <dyeguitto@hotmail.com>
2020-01-10 15:49:34 -0300

    Add action widgets_init

--- index.php
+++ index.php
@@ -35,6 +35,6 @@ add_action( 'wp_enqueue_scripts', 'r_enqueue_scripts', 100 );
 add_action( 'wp_ajax_r_rate_recipe', 'r_rate_recipe' );
 add_action( 'wp_ajax_nopriv_r_rate_recipe', 'r_rate_recipe' );
 add_action( 'admin_init', 'recipe_admin_init' );
-
+add_action( 'widgets_init', 'r_widgets_init' );
 
 // Shortcodes
