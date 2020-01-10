94369e8fdfaa99be3c66c892babf583104ad8dda
Diego Mandu <dyeguitto@hotmail.com>
2020-01-10 20:01:28 -0300

    Display daily recipe content in the frontend

--- includes/widgets/daily-recipe.php
+++ includes/widgets/daily-recipe.php
@@ -59,7 +59,42 @@ public function update( $new_instance, $old_instance ){
 */
 public function widget( $args, $instance ){
 	// outputs the content of the widget
+	extract( $args );
+	extract( $instance );
 
+	$title 			= apply_filters( 'widget_title', $title );
+
+	echo $before_widget;
+	echo $before_title . $title . $after_title;
+
+	$recipe_id 		= get_transient( 'r_daily_recipe' );
+
+	if( !$recipe_id ){
+		$recipe_id    =  r_get_random_recipe();
+
+		set_transient(
+			'r_daily_recipe',
+			$recipe_id,
+			DAY_IN_SECONDS
+		);
+	}
+
+	?>
+
+	<div class="portfolio-image">
+		<a href="<?php echo get_permalink( $recipe_id ); ?>">
+			<?php echo get_the_post_thumbnail( $recipe_id, 'thumbnail' ); ?>
+		</a>
+	</div>
+	<div>
+		<h3><a href="<?php echo get_permalink( $recipe_id ); ?>">
+			<?php echo get_the_title( $recipe_id ); ?>
+		</a></h3>
+	</div>
+	<?php
+
+
+	echo $after_widget;
 
 }
 
