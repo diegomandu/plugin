faa9e1f254b8c70bc8b172ee82d1913cd8ee735a
Diego Mandu <dyeguitto@hotmail.com>
2020-01-10 16:47:01 -0300

    Add widget description

--- includes/widgets/daily-recipe.php
+++ includes/widgets/daily-recipe.php
@@ -1,6 +1,27 @@
 <?php
 
-class R_Daily_Recipe_Widget(){
+class R_Daily_Recipe_Widget extends WP_Widget{
+/**
+* Sets up the widgets name etc
+*/
+public function __construct(){
+	$widget_ops = array(
+		'description'	=>	'Display a random recipe each day.',
+	);
+	parent::__construct( 'r_daily_recipe_widget', 'Recipe of the Day', $widget_ops );
+}
 
-	
+}
+
+
+/**
+*
+* Outputs the content of the widget
+*
+*@param array $args
+*@param array $instance
+*/
+public function widget( $args, $instance ){
+	// outputs the content of the widget
+
 }
