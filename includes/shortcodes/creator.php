dead6987106f8145fa2516566e22476fb41bce21
Diego Mandu <dyeguitto@hotmail.com>
2020-01-11 16:37:47 -0300

    Define function r_generate_content_editor

--- includes/shortcodes/creator.php
+++ includes/shortcodes/creator.php
@@ -3,8 +3,19 @@
 function r_recipe_creator_shortcode(){
 	$creatorHTML		=	file_get_contents('creator-template.php', true);
 
-	wp_editor( '', 'recipecontenteditor' );
+	
 
 	return $creatorHTML;
+}
+
+/*	PICKUP THE TINYMCE EDITOR USING BUFFER BEFORE IT DISPLAYS, 
+	SO WE CAN CONTROL AND INSERT INSIDE THE FORM
+ */
+
+function r_generate_content_editor(){
+	ob_start();
+	wp_editor( '', 'recipecontenteditor' );
+	$editor_contents		= ob_get_clean();
+	return $editor_contents;
 }
 
