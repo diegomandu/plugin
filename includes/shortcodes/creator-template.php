4966209fb7c3ee85886ee9cba752654aacf08ddb
Diego Mandu <dyeguitto@hotmail.com>
2020-01-11 16:28:20 -0300

    Replace form input HTML for placeholder

--- includes/shortcodes/creator-template.php
+++ includes/shortcodes/creator-template.php
@@ -3,10 +3,7 @@
         <label>Title</label>
         <input type="text" class="form-control" id="r_inputTitle">
     </div>
-    <div class="form-group">
-        <label>Content</label>
-        <textarea class="form-control" id="recipecontenteditor"></textarea>
-    </div>
+    CONTENT_EDITOR
     <div class="form-group">
         <button type="submit" class="btn btn-primary">Submit Recipe</button>
     </div>
