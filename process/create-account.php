7bec07d57ee5391ec93de20dc7090974d291df6d
Diego Mandu <dyeguitto@hotmail.com>
2020-01-15 18:02:17 -0300

    Process AJAX response and sanitize data

--- process/create-account.php
+++ process/create-account.php
@@ -1,6 +1,24 @@
 <?php 
 
 function recipe_create_account(){
+	$output				= ['status' => 1];
+	$nonce				= isset($_POST['_wpnonce']) ? $_POST['_wpnonce'] : '';
 
-	
+	if( !wp_verify_nonce( $nonce, 'recipe_auth' ) ){
+		wp_send_json( $output );
+	} 
+
+	if( !isset($_POST['username'], $_POST['email'], $_POST['pass'], $_POST['confirm_pass']) ){
+		wp_send_json( $output );
+	}
+
+	$name				=	sanitize_text_field( $_POST['name'] );
+	$username			=	sanitize_text_field( $_POST['username'] );
+	$email				=	sanitize_email( $_POST['email'] );
+	$pass 				=	sanitize_text_field( $_POST['pass'] );
+	$confirm_pass		=	sanitize_text_field( $_POST['confirm_pass'] );
+
+	if( username_exists($username) || email_exists($email); || $pass != $confirm_pass || !is_email($email) ){
+		wp_send_json($output);
+	}
 }
