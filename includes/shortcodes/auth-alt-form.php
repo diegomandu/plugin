f9094f1281bbac4a805a5f78ed8a04da6686379a
Diego Mandu <dyeguitto@hotmail.com>
2020-01-16 14:25:21 -0300

    Display HTML login form

--- includes/shortcodes/auth-alt-form.php
+++ includes/shortcodes/auth-alt-form.php
@@ -1,6 +1,18 @@
 <?php 
 
 function r_recipe_auth_alt_form_shortcode(){
+	if( is_user_logged_in() ){
+		return '';
+	}
 
-	
+	$formHTML			=	'<div class="col_one_third nobottommargin">';
+
+	$formHTML			.=	wp_login_form([
+		'echo'			=>	false,
+		'redirect'		=> 	home_url( '/' )	
+	]);
+
+	$formHTML			.=	'</div>';
+
+	return $formHTML;
 } 
