281e595242fe21a7776c2f272e1978ee510680a3
Diego Mandu <dyeguitto@hotmail.com>
2020-01-16 16:53:10 -0300

    Display logout link

--- includes/front/logout-link.php
+++ includes/front/logout-link.php
@@ -1,5 +1,10 @@
 <?php 
 
-function ju_new_nav_menu_items(){
-	
+function ju_new_nav_menu_items( $items ){
+	if( !is_user_logged_in() ){
+		return $items;
+	}
+
+	$new_link			= "<li>" . wp_loginout( home_url( '/' ), false ) . "</li>";
+	return $items . $new_link;
 }
