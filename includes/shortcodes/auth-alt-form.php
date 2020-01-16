9b3c5ebac38adb925d58cbead2b4931c10309569
Diego Mandu <dyeguitto@hotmail.com>
2020-01-16 15:07:57 -0300

    Process the authentication

--- includes/shortcodes/auth-alt-form.php
+++ includes/shortcodes/auth-alt-form.php
@@ -16,3 +16,22 @@ function r_recipe_auth_alt_form_shortcode(){
 
 	return $formHTML;
 } 
+
+function r_alt_authenticate( $user, $username, $password ){
+	if( $_SERVER['REQUEST_METHOD'] !== 'POST' ){
+		return $user;
+	}
+
+	if( !is_wp_error($user) ){
+		return $user;
+	}
+
+	$error_codes		= join( ',', $user->get_error_codes() );
+	$login_url			= home_url( 'account' );
+	$login_url			= add_query_arg(
+		'login', $error_codes, $login_url
+	);
+
+	wp_redirect($login_url);
+	exit;
+}
