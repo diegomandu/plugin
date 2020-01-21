75bd59dc5f55dac27e94df513cecf764d2f14b4e
Diego Mandu <dyeguitto@hotmail.com>
2020-01-21 15:11:01 -0300

    Include enqueue.php

--- includes/admin/init.php
+++ includes/admin/init.php
@@ -2,6 +2,7 @@
 
 function recipe_admin_init(){
 	include( 'column.php' );
+	include( 'enqueue.php' );
 
 	add_filter( 'manage_recipe_posts_columns', 'r_add_new_recipe_columns' );
 	add_action( 'manage_recipe_posts_custom_column', 'r_manage_recipe_columns', 10, 2 );
