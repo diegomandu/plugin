8b969b5234915c733734a95dd76859e8e00f6126
Diego Mandu <dyeguitto@hotmail.com>
2020-01-21 17:25:56 -0300

    Call r_settings_api

--- includes/admin/init.php
+++ includes/admin/init.php
@@ -3,10 +3,12 @@
 function recipe_admin_init(){
 	include( 'column.php' );
 	include( 'enqueue.php' );
-	include( 'settings-api.php' )
+	include( 'settings-api.php' );
 
 	add_filter( 'manage_recipe_posts_columns', 'r_add_new_recipe_columns' );
 	add_action( 'manage_recipe_posts_custom_column', 'r_manage_recipe_columns', 10, 2 );
 	add_action( 'admin_enqueue_scripts', 'r_admin_enqueue' );
 	add_action( 'admin_post_r_save_options', 'r_save_options' );
+
+	r_settings_api();
 }
