c73baca62e16acd711a33fb4d7f0f8a26c2ed3c1
Diego Mandu <dyeguitto@hotmail.com>
2020-01-21 16:44:13 -0300

    Make the fields Dynamic inside the form

--- includes/admin/options-page.php
+++ includes/admin/options-page.php
@@ -1,11 +1,20 @@
 <?php 
 
 function r_plugin_opts_page(){
+	$recipe_opts										= get_option( 'r_opts' );
+
 	?>
 	<div class="wrap">
 		<div class="card">
 			<div class="card-body">
 				<h3 class="card-title"><?php _e('Recipe Settings', 'recipe'); ?></h3>
+				<?php 
+				if($_GET['status'] == 1 ){
+				?>
+					<div class="alert alert-success"> Options updated Sucessfully! </div>
+				<?php
+				}  
+				?>
 				<form method="POST" action="admin-post.php">
 					<input type="hidden" name="action" value="r_save_options">
 					<?php wp_nonce_field( 'r_options_verify' ); ?>
@@ -13,14 +22,16 @@ function r_plugin_opts_page(){
 						<label><?php _e('User login required for rating recipes', 'recipe'); ?></label>
 						<select class="form-control" name="r_rating_login_required">
 							<option value="1">No</option>
-							<option value="2">Yes</option>
+							<option value="2"
+								<?php echo $recipe_opts['rating_login_required'] == 2 ? 'SELECTED' : '' ?>>Yes</option>
 						</select>
 					</div>
 					<div class="form-group">
 						<label><?php _e('User login required for submitting recipes', 'recipe'); ?></label>
 						<select class="form-control" name="r_submission_login_required">
 							<option value="1">No</option>
-							<option value="2">Yes</option>
+							<option value="2"
+								<?php echo $recipe_opts['recipe_submission_login_required'] == 2 ? 'SELECTED' : '' ?>>Yes</option>
 						</select>
 					</div>
 					<div class="form-group">	
